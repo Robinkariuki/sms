87de3c5d6ad295e0b6fedab264d9a9cc54e01274
unknown <robinkaris7@gmail.com>
2021-11-11 10:45:33 +0300

    made chages

--- src/components/Form.js
+++ src/components/Form.js
@@ -78,7 +78,7 @@ export const Forms = () => {
 
      }).then(res=>{
       setRecepientsDB(res.data.recordset)
-      // console.log(res.data.recordset)
+      
       const data = []
      const contacts = []
       res.data.recordset.map(obj=>{
@@ -88,7 +88,7 @@ export const Forms = () => {
         data.push(Object.values(obj))
         setRecepientsDB(data)
         setRow(data)
-        setCol(Object.keys(obj))
+        return setCol(Object.keys(obj))
         
        
 
@@ -105,82 +105,64 @@ export const Forms = () => {
     
       
       
-      
-
-     
-
-
       const formData = {
-        shortCode:"Naivas",
+       recipients,
+       message
+      
+      }
+     axios({
+       method:'post',
+       url:"http://localhost:3001/api/smsbulk",
+       data: formData,
+     })
+     .then(resp=>{
+       console.log(resp)
+
+      alert(resp.data)
+      setCol([])
+      setRow([])
+      setMessage('')
+      setRecepients('')
+      setCount(0)
+      setDescription('')
+
+
+      const dataFromForm ={
         recipients,
-        message
+        message,
+        description,
 
       }
-
-
-
-
-
+  
+      const data = JSON.stringify(dataFromForm)
       axios({
-          method: "post",
-          url: "/sms/v1/bulk/api_submit",
-          data: formData,
-          headers: {
-            "Content-Type": "application/json",
-            "X-Authorization-Key": "Q2ZtR0N5ODYrazlWWTZtUWtBUnJTN0NKejljeE1FNDFTSVNhZWh0SjJ4Y2NPZ3dtZ0g0N1dCdHNKTm1PSThVY3JnQkl4UzFaS1VVWGtBOXZKdWZXcE5tMFFyMWY1L2JuaWFqdTVzWlQ0bVArTElTYXZHNDJJS0p6d1dEUC9Rb3c1TDNhR0lYanBSSU8wM3FJTTVhOVRHakJLSW80QkFMUzZuL0JUZnJNMXJRc2lEc3FxS3dSS0cxRHB6Yk4ya3F0QVVOZGRYZDZaU0QrTUJTQXo1VE1UWmlic3JpNjFmaE44UWdSaGIvN3lpTDFDeVJyaExDa1ZKTlM0QW9BTHJrY2VLUzFyU2JoVXh1WlBIVGZxTnlEVVBkYTM4MkUvVmlQN0xwNWZqdmkyWEtMSjd3UnFjSW1zTlF3Mzc1OXhEK1BVYmZMNWl0Rm9JQUNyNHllZkkzVXVBPT06OhHjXO9gzExEEjwZ3iZjJXo=",
-            "X-Requested-With": "XMLHttpRequest",
-          },
-        })
-        .then(resp=>{
-          alert(resp.data.statusDescription)
-          setCol([])
-          setRow([])
-          setMessage('')
-          setRecepients('')
-          setCount(0)
-          setDescription('')
+        method:"post",
+        url:'http://localhost:3001/api/log',
+        data,
+        headers: {
+          "Content-Type": "application/json"
         
+        },
 
-
-
-
-
-          const dataFromForm ={
-            recipients,
-            message,
-            description,
-    
-          }
+      })
+      .then(res=>{
+        alert(res.data)
+     
+      })
+      .catch(err =>{
+        
+        alert(err)
       
-          const data = JSON.stringify(dataFromForm)
-          axios({
-            method:"post",
-            url:'http://localhost:3001/api/log',
-            data,
-            headers: {
-              "Content-Type": "application/json"
-            
-            },
-    
-          })
-          .then(res=>{
-            alert(res.data)
-            console.log(res)
-          })
-          .catch(err =>{
-            
-            alert(err)
-            console.log(err)
-    
-          })
 
-        })
-        .catch(err =>{
-       
-          alert(err.response.data.data.message)
+      })
 
-        })
-        
+    })
+    .catch(err =>{
+   
+      alert(err.response.data.data.message)
+
+    
+     })
 
 
     };
